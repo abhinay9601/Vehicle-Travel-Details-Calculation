7fc7c28d730f85b761c5644e54cf20fbd0d79982
Sibashis216 <66904347+Sibashis216@users.noreply.github.com>
2024-10-06 01:26:56 +0530

    Update server.js

--- Backend /server.js
+++ Backend /server.js
@@ -1,1 +1,21 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+
+const PORT = process.env.PORT;
+const routes = require('./routes/Routes');
+
+app.use(express.json({ limit: '50mb' }));
+app.use(cors());
+
+require('./db');
+
+app.use('/api', routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
